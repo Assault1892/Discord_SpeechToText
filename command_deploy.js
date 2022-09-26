1ba1c1c90962ddf535145a0b309e105c00d7057e
Assault.8448 <vvvvvgaga12356@yahoo.co.jp>
2022-09-26 12:37:29 +0900

    eoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeoeo

--- command_deploy.js
+++ command_deploy.js
@@ -1,25 +1,34 @@
+const { REST, Routes } = require("discord.js");
+const { token, clientId, guildId } = require("./config.json");
 const fs = require("node:fs");
-const path = require("node:path");
-const { SlashCommandBuilder, Routes } = require("discord.js");
-const { REST } = require("@discordjs/rest");
-const { clientId, guildId, token } = require("./config.json");
 
 const commands = [];
-const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
-  .readdirSync(commandsPath)
+  .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-
-  commands.push(command);
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: "10" }).setToken(token);
 
-rest
-  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
+    );
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
